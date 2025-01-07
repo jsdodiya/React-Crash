2ab7469e848605e18a738494b3c2d35e7fa736cd
jsdodiya <jinendradodiya9@gmail.com>
2025-01-07 16:13:31 +0530

    jobs part done

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 import Jobs from '../jobs.json';
 import JobListing from './JobListing';
 
-const JobListings = () => {
-    const recentJobs = Jobs.slice(0, 3)
+const JobListings = ({ isHome = false}) => {
+
+    const jobListings = isHome ? Jobs.slice(0, 3) : Jobs
     
     return (
         <div>
             <section className='bg-blue-50 px-4 py-10'>
                 <div className='container-xl lg:container m-auto'>
-                    <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>Browse Jobs</h2>
+                    <h2 className='text-3xl font-bold text-indigo-500 mb-6 text-center'>{isHome ? 'Recent Jobs' : 'All Jobs'}</h2>
                     <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-                        {recentJobs.map((job) => (
+                        {jobListings.map((job) => (
                             <JobListing key={job.id} job={job}/>
                            
                         ))}
