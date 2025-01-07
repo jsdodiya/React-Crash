85001e8af981df6d2b2d9b8f81445a284e9d4467
jsdodiya <jinendradodiya9@gmail.com>
2025-01-07 15:43:40 +0530

    nav bar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import {Route, createBrowserRouter, 
+createRoutesFromElements,RouterProvider } from 'react-router-dom'
+import HomePage from './pages/HomePage';
+import MainLayout from './layouts/MainLayout';
+import JobsPages from './pages/JobsPages';
+import NotFoundPage from './pages/NotFoundPage';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+  <Route path='/' element={<MainLayout />} >
+  <Route index element={<HomePage/>}/>
+  <Route path='/jobs' element={<JobsPages />} />
+  <Route path='*' element={<NotFoundPage />} />
+  </Route>
+  )
+)
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero/>
-      <HomeCards/>
-      <JobListings/>
-      <ViewAllJobs/>
-    </>
-  );
+  return <RouterProvider router={router}/>
 };
 
 export default App;
